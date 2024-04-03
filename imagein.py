1cebb4907f53a50f82df96a800733006d9adc500
Shubham-joshi20 <s23.joshi@gmail.com>
2024-04-03 19:55:26 +0530

    Added sharping

--- imagein.py
+++ imagein.py
@@ -46,6 +46,27 @@ def adjust_contrast(image_path):
 
     return adjusted_image
 
+def sharpen_image(image_path, sharpen=True):
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Check if the image is color (3 channels)
+    if len(image.shape) == 3:
+        # Create a sharpening kernel
+        kernel = np.array([[-1, -1, -1],
+                           [-1,  9, -1],
+                           [-1, -1, -1]])
+        # Apply the sharpening kernel
+        sharpened_image = cv2.filter2D(image, -1, kernel)
+    else:
+        # Apply sharpening directly to grayscale image
+        sharpened_image = cv2.filter2D(image, -1, np.array([[0, -1, 0],
+                                                             [-1, 5, -1],
+                                                             [0, -1, 0]]))
+
+    return sharpened_image
+
+
 @app.route('/')
 def index():
     return render_template('Front_page.html')
@@ -53,6 +74,10 @@ def index():
 @app.route('/input.html')
 def input_page():
     return render_template('input.html')
+
+@app.route('/Sharping.html')
+def Sharp_page():
+    return render_template('Sharping.html')
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
@@ -77,11 +102,32 @@ def process_image():
 
     return 'No image selected'
 
+
+@app.route('/sharp_image', methods=['POST'])
+def sharp_image():
+    if 'image' in request.files:
+        image = request.files['image']
+        if image.filename != '':
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+            image.save(image_path)
+        
+        sharp_img = sharpen_image(image_path,sharpen=True)
+
+        sharp_image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'adjusted_' + image.filename)
+        cv2.imwrite(sharp_image_path, sharp_img)
+
+        # Prepare the image for download
+        with open(sharp_image_path, 'rb') as image_file:
+            img_data = base64.b64encode(image_file.read()).decode()
+
+        return render_template('download.html', image_data=img_data)
+
 @app.route('/download_image', methods=['POST'])
 def download_image():
     image_data = request.form['image_data']
     img_data = base64.b64decode(image_data)
     return send_file(BytesIO(img_data), mimetype='image/jpeg', as_attachment=True, download_name='adjusted_image.jpeg')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
